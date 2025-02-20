5cea6b64d4666690b0ae90411c486d47b0e9ad56
Starsh <starshmail@yandex.ru>
2025-02-20 13:44:01 +0000

    Редактирование скрипта bash-git-promt.sh

--- bash-git-promt.sh
+++ bash-git-promt.sh
@@ -1,9 +1,12 @@
 #/bin/bash
-apt install git
+sudo apt install git -y
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-cat echo "#Informative git prompt for bash and fish
+cat >> $HOME/.bashrc <<EOF
+#Informative git prompt for bash and fish
 #Execute git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
-    GIT_PROMPT_ONLY_IN_REPO=1
-    source "$HOME/.bash-git-prompt/gitprompt.sh"
-fi" >> .bashrc
+if [ -f "\$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1c
+    source "\$HOME/.bash-git-prompt/gitprompt.sh"
+fi
+EOF
+source ~/.bashrc
